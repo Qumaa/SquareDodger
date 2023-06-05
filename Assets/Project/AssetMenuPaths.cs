9958822fd849562d735b04f6522779b0ee567838
Qma <mk348437@gmail.com>
2023-06-05 23:04:29 +0200

    started color settings; created respective scriptable object class, instance and references;

--- Assets/Project/AssetMenuPaths.cs
+++ Assets/Project/AssetMenuPaths.cs
@@ -3,14 +3,18 @@ namespace Project
     public static class AssetMenuPaths
     {
         private const string _PROJECT = "Project/";
-        
+
         private const string _OBSTACLES = _PROJECT + "Obstacles/";
         public const string OBSTACLE_SPAWNER = _OBSTACLES + "Spawner Config";
         public const string OBSTACLE_MANAGER_CONFIG = _OBSTACLES + "Manager Config";
+        
+        private const string _VISUALS = _PROJECT + "Visuals/";
+        public const string VISUALS_CONFIG = _VISUALS + "Visuals Config";
+        public const string GAME_COLORS_CONFIG = _VISUALS + "Game Colors Config";
 
         public const string CAMERA_CONFIG = _PROJECT + "Camera Config";
         public const string PLAYER_CONFIG = _PROJECT + "Player Config";
         public const string GAME_CONFIG = _PROJECT + "Game Config";
-        public const string VISUALS_CONFIG = _PROJECT + "Visuals Config";
+        
     }
 }
---
+++ Assets/Project/Configs/Source scripts/GameColorsConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Project.Game
+{
+    [CreateAssetMenu(menuName = AssetMenuPaths.GAME_COLORS_CONFIG, fileName = "New Game Colors Config")]
+    public class GameColorsConfig : ScriptableObject
+    {
+        [SerializeField] private Color _playerColor;
+        [SerializeField] private Color _obstaclesColor;
+        [SerializeField] private Color _backgroundColor;
+        [SerializeField] private Color _backgroundParticlesColor;
+    }
+}
