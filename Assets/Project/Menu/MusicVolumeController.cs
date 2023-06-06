c9740135c6894474aa1f1cfcfee33ca0cf97ea59
TheRiddler1488 <eazyshotlocket@gmail.com>
2023-06-07 02:17:43 +0300

    Music player

---
+++ Assets/Project/Menu/MusicVolumeController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project
+{
+    public class MusicVolumeController : MonoBehaviour
+    {
+        public Slider volumeSlider;
+        public AudioSource musicSource;
+
+        private void Start()
+        {
+           
+            volumeSlider.value = musicSource.volume;
+
+           
+            volumeSlider.onValueChanged.AddListener(UpdateMusicVolume);
+        }
+
+        private void UpdateMusicVolume(float volume)
+        {
+           
+            musicSource.volume = volume;
+        }
+    }
+}
