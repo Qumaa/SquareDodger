fa86b920c267a5961145758cb572f7c996bd99a2
Qma <mk348437@gmail.com>
2023-06-12 14:29:30 +0200

    reworked settings menu from scratch;

--- Assets/Project/Menu/UI scripts/SettingsMenuHandler.cs
+++ Assets/Project/Menu/UI scripts/SettingsMenuHandler.cs
@@ -7,17 +7,18 @@ namespace Project.UI
     public class SettingsMenuHandler : GameCanvasUI, ISettingsMenu
     {
         [SerializeField] private Button _closeSettingsButton;
-
+        [SerializeField] private Button _backgroundButton;
+        
         public event Action OnClosePressed;
 
         protected override void OnAwake()
         {
             _closeSettingsButton.onClick.AddListener(HandleCloseSettingsPressed);
+            _backgroundButton.onClick.AddListener(HandleCloseSettingsPressed);
         }
 
         private void HandleCloseSettingsPressed()
         {
-            Hide();
             OnClosePressed?.Invoke();
         }
     }
